60136214514ba5a520f386e2ed1d3f9c2c04f557
Jasper Hjorth <jasper.hjorth@gmail.com>
2017-05-25 13:37:40 +0300

    Add support for storing Swagger specs

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import *
+from werkzeug import secure_filename
+import os
 
-# make sure you have main.db file in the same directory and it has proper rights
-# you can make the file by running following commands in your command line:
-# touch main.db
-# chmod 700 main.db
+#Please read the instructions in README.md to get this work. 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db = SQLAlchemy(app)
 
-
-class swagger(db.Model):
+class Swagger(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    swagger = db.Column(db.LargeBinary(), unique=True)
+    swagger = db.Column(db.String)
     
     def __init__(self, swagger):
         self.swagger = swagger
 
     def __repr__(self):
-        return '<swagger %r>' % self.swagger
+        return '<Swagger %r>' % self.swagger
 
 
+@app.route('/', methods=['POST'])
+def upload():
+    temp = request.files['file']
+    swagger = temp.read()
+    db.session.add(Swagger(swagger))
+    db.session.commit()
+    return 'Added swagger to the database'
 
+
+app.run(debug = True)
